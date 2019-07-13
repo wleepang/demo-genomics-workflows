a3412c24da920b71447e4c8f09166953e31de11c
Pang, Lee <pwyming@.amazon.com>
2019-07-13 14:36:54 -0500

    add environment setup script
    this script bootstraps the environment since that can't be
    done via cloudformation

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# core requirements
+sudo yum install -y jq
+
+# nextflow requires java 8
+sudo yum install -y java-1.8.0-openjdk
+
+# The Cloud9 AMI is Amazon Linux 1, which defaults to Java 7
+# change the default to Java 8 and set JAVA_HOME accordingly
+sudo alternatives --set java /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java
+export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk.x86_64
+
+# get and install nextflow
+mkdir ~/bin
+cd ~/bin
+curl -s https://get.nextflow.io | bash
+
+# create a nextflow config for aws-batch
+CONFIG_DIR=~/environment/config
+mkdir $CONFIG_DIR
+DEFAULT_JOB_QUEUE=$(aws batch describe-job-queues | jq -r .jobQueues[].jobQueueName | grep default)
+
+cat <<EOF > $CONFIG_DIR/batch.config
+process.executor = "awsbatch"
+process.queue = "${DEFAULT_JOB_QUEUE}"
+executor.awscli = "/home/ec2-user/miniconda/bin/aws"
+EOF
