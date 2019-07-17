304a31f3786ae46d50b8e981d169443cc3fc55b8
Pang, Lee <pwyming@.amazon.com>
2019-07-16 23:36:15 -0400

    regionalize

--- scripts/nextflow/submit-job.sh
+++ scripts/nextflow/submit-job.sh
@@ -3,8 +3,10 @@
 WORKFLOW_NAME=$1  # custom name for workflow
 OVERRIDES=$2      # path to json file for job overrides, e.g. file://path/to/overrides.json
 
+AWS_REGION=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone| sed -r "s/(.*?)[a-z]/\1/")
+
 # get the name of the high-priority queue
-HIGHPRIORITY_JOB_QUEUE=$(aws batch describe-job-queues | jq -r .jobQueues[].jobQueueName | grep highpriority)
+HIGHPRIORITY_JOB_QUEUE=$(aws --region $AWS_REGION batch describe-job-queues | jq -r .jobQueues[].jobQueueName | grep highpriority)
 
 # submits nextflow workflow to AWS Batch
 aws batch submit-job \
